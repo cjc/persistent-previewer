befa21525c69ee536b1d67211a05ff71470cf254
cjc <cjc@cjc.id.au>
2010-01-19 22:53:39 +1000

    Initial checkin, working skeleton

---
+++ content.js
@@ -0,0 +1,14 @@
+//alert("replacing");
+var tunnel = document.createElement("DIV");
+tunnel.id = "persistentpreviewtunnel";
+document.body.appendChild(tunnel);
+
+tunnel.addEventListener('PreviewTunnelEvent', function() {
+	var eventData = tunnel.innerText;
+	//alert("contnt script has data " + eventData);
+	chrome.extension.sendRequest({message: "Preview", values: eventData});
+});
+
+var scr = document.createElement("SCRIPT");
+scr.src = chrome.extension.getURL("inject.js");
+document.body.appendChild(scr);
---
+++ inject.js
@@ -0,0 +1,10 @@
+//alert("1.21 gigawatts");
+
+var customEvent = document.createEvent('Event');
+customEvent.initEvent('PreviewTunnelEvent', true, true);
+
+function PreviewTrack(id, element) {
+	hiddenDiv = document.getElementById('persistentpreviewtunnel');
+	hiddenDiv.innerText = id
+	hiddenDiv.dispatchEvent(customEvent);
+}
